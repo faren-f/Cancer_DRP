4b73a3387bae96943252a5f1e31ac0c38738b201
Faren <87604643+Firoozbakht@users.noreply.github.com>
2022-08-03 10:47:20 +0200

    writing general framework

--- R/Prepare_Data/General_Framework.R
+++ R/Prepare_Data/General_Framework.R
@@ -1,5 +1,32 @@
 rm(list=ls())
 setwd("~/Desktop/Cancer_DRP/R/Prepare_Data/")
 
+GE = readRDS("Processed_Data/Step1/expresion_matrix.rds")
+sen = readRDS("Processed_Data/Step1/sensitivity_matrix.rds")
+dim(GE)
+dim(sen)
+Rep = 1
 
-
+mean_mse = rep(0,ncol(sen))
+sd_mse = rep(0,ncol(sen))
+mean_corr = rep(0,ncol(sen))
+sd_corr = rep(0,ncol(sen))
+for (i in ncol(sen)){
+  
+  GE_i = GE[!is.na(sen[,i]),]
+  dim(GE_i)
+  sen_i = sen_i[!is.na(sen[,i])]
+  length(sen_i)
+  
+  mse = rep(0,Rep)
+  corr = rep(0,Rep)
+  for(j in 1:Rep){
+    
+    
+    
+    
+    
+  }
+  
+  
+}
